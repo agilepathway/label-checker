75870c945c95a37b0a5b8f7f94766b8bd7a2b78c
John Boyes <john@agilepathway.co.uk>
2023-02-21 05:17:39 +0000

    Add prefix mode to label checks (#333)
    This is a new feature that checks for the presence or absence of labels
    starting with a given prefix.
    
    This prefix label checking is a powerful feature that allows pull
    requests to have scoped labels [in a similar way to GitLab][1].
    
    To use, set the `prefix_mode` as an input parameter in the
    `with` section: `prefix_mode: true`.
    It is an optional parameter as it defaults to false.
    
    Any string can be used as the prefix.
    
    `one_of`, `any_of`, `none_of` can be used when checking prefixes, but
    `all_of` cannot (as `all_of` does not make sense when checking for
    labels starting with a given prefix).
    
    Only one prefix can be specified at a time. For instance
    `one_of: "type:","visibility/"` is not allowed.
    
    Closes #328
    
    [1]: https://docs.gitlab.com/ee/user/project/labels.html#scoped-labels

--- internal/github/pullrequest/labels.go
+++ internal/github/pullrequest/labels.go
@@ -13,32 +13,36 @@ type Labels []string
 
 // HasExactlyOneOf indicates whether the labels contain exactly
 // one of the specified labels, along with a report describing the result.
-func (l Labels) HasExactlyOneOf(specified []string) (bool, string) {
-	return l.hasXof(specified, "1")
+func (l Labels) HasExactlyOneOf(specified []string, prefixMode bool) (bool, string) {
+	return l.hasXof(specified, "1", prefixMode)
 }
 
 // HasNoneOf indicates whether the labels contain
 // none of the specified labels, along with a report describing the result.
-func (l Labels) HasNoneOf(specified []string) (bool, string) {
-	return l.hasXof(specified, "none")
+func (l Labels) HasNoneOf(specified []string, prefixMode bool) (bool, string) {
+	return l.hasXof(specified, "none", prefixMode)
 }
 
 // HasAllOf indicates whether the labels contain
 // all of the specified labels, along with a report describing the result.
-func (l Labels) HasAllOf(specified []string) (bool, string) {
-	return l.hasXof(specified, "all")
+func (l Labels) HasAllOf(specified []string, prefixMode bool) (bool, string) {
+	if prefixMode {
+		return false, "The label checker does not support prefix checking with `all_of`, as that is not a logical combination."
+	}
+	return l.hasXof(specified, "all", prefixMode)
 }
 
 // HasAnyOf indicates whether the labels contain
 // any of the specified labels, along with a report describing the result.
-func (l Labels) HasAnyOf(specified []string) (bool, string) {
-	return l.hasXof(specified, "any")
+func (l Labels) HasAnyOf(specified []string, prefixMode bool) (bool, string) {
+	return l.hasXof(specified, "any", prefixMode)
 }
 
 type labelCheck struct {
-	Specified []string
-	Found     []string
-	CheckFor  string
+	Specified  []string
+	Found      []string
+	CheckFor   string
+	PrefixMode bool
 }
 
 func (v labelCheck) IsValid() bool {
@@ -62,7 +66,7 @@ func (v labelCheck) NumberFound() int {
 	return len(v.Found)
 }
 
-func (l Labels) hasXof(specified []string, checkFor string) (bool, string) {
+func (l Labels) hasXof(specified []string, checkFor string, prefixMode bool) (bool, string) {
 	var (
 		labelCheckMsgBuilder strings.Builder
 		foundLabels          []string
@@ -72,19 +76,26 @@ func (l Labels) hasXof(specified []string, checkFor string) (bool, string) {
 		"Label check " +
 		"{{if .IsValid}}successful{{else }}failed{{end}}: " +
 		"required {{.CheckFor}} " +
-		"of {{range $s := .Specified}}{{$s}}, {{end}}" +
+		"{{if .PrefixMode}}prefixed with{{else }}of{{end}} " +
+		"{{range $s := .Specified}}{{$s}}, {{end}}" +
 		"{{if .IsValid}}and{{else }}but{{end}} " +
 		"found {{.NumberFound}}" +
 		"{{if .NumberFound}}: {{else }}.{{end}}" +
 		"{{range $i, $f := .Found}}{{if $i}}, {{end}}{{$f}}{{end}}"))
 
 	for i := 0; i < len(l); i++ {
-		if slice.Contains(specified, l[i]) {
-			foundLabels = append(foundLabels, l[i])
+		if prefixMode {
+			if slice.StartsWithAnyOf(specified, l[i]) {
+				foundLabels = append(foundLabels, l[i])
+			}
+		} else {
+			if slice.Contains(specified, l[i]) {
+				foundLabels = append(foundLabels, l[i])
+			}
 		}
 	}
 
-	check := labelCheck{specified, foundLabels, checkFor}
+	check := labelCheck{specified, foundLabels, checkFor, prefixMode}
 
 	panic.IfError(t.Execute(&labelCheckMsgBuilder, check))
 
